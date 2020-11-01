2a6490f7796684d4872b6f7b83cf5316ea6c57bc
szihai <szihai@gmail.com>
2020-11-01 01:51:26 -0700

    add action

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+curl -sL https://github.com/oam-dev/kubevela/releases/download/v0.0.8/vela-v0.0.8-linux-amd64.tar.gz | \
+  tar xz && mv linux-amd64/vela /usr/local/bin/vela && chmod +x /usr/local/bin/vela 
+
+echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
+export KUBECONFIG=kubeconfig
+
+vela up 
+
+bash -c "set -e;  set -o pipefail; $1"

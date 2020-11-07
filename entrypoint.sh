d50122d8fc93fe3797a1f3a6eea3d14f5117bb39
szihai <szihai@gmail.com>
2020-11-06 23:03:01 -0800

    change to 0.09

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-curl -sL https://github.com/oam-dev/kubevela/releases/download/v0.0.8/vela-v0.0.8-linux-amd64.tar.gz | \
+curl -sL https://github.com/oam-dev/kubevela/releases/download/v0.0.9/vela-v0.0.9-linux-amd64.tar.gz | \
   tar xz && mv linux-amd64/vela /usr/local/bin/vela && chmod +x /usr/local/bin/vela 
 
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
